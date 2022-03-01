e632e01fae0e3f77f20009876b203f953031fc9e
utkarsh aggarwal <utkarsh aggarwal>
2022-03-01 13:42:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text("30 days of flutter"),
+        ),
+      ),
+    );
+  }
+}
